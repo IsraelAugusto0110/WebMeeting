48e6a6f66b92cb52c39cb9cd70ae085cfab2f58d
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2021-03-28 20:04:49 -0300

    novo
    Criado por Thiago Canonici

---
+++ BackEnd/iacit1/database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE funcionario (
+  Id int(11) NOT NULL AUTO_INCREMENT,
+  Participante varchar(300) NOT NULL,
+  Area varchar(300) NOT NULL,
+  Email varchar(300) NOT NULL,
+  Telefone varchar(200) NOT NULL,
+  PRIMARY KEY (Id)
+);
