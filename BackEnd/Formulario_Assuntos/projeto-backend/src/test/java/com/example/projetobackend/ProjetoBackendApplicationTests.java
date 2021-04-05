92f1ce27a6613a364603462b5af8a0116c4332c5
IsraelAugusto0110 <56437566+IsraelAugusto0110@users.noreply.github.com>
2021-04-05 20:01:35 -0300

    Formulario para a criação de assuntos
    Criado protótipo do formulário para inserção de novos assuntos na ata.  Faltam algumas correções do tipo da variável "prazo" da classe "AssuntoAta" para data,, optei num primeiro momento por usar String. Necessário ainda acrescentar função para criar uma tabela sql temporária para que os dados possam ser editados e também função de salvar, que ira enviar as informações para um arquivo excel e depois para pdf. Falta ainda a devida documentação do código.

---
+++ BackEnd/Formulario_Assuntos/projeto-backend/src/test/java/com/example/projetobackend/ProjetoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
