6f4b14e5987b37e780b68aa52642785a7bd94570
IsraelAugusto0110 <56437566+IsraelAugusto0110@users.noreply.github.com>
2021-04-13 15:14:00 -0300

    Delete database.sql

--- BackEnd/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE funcionario (
-  Id int(11) NOT NULL AUTO_INCREMENT,
-  Participante varchar(300) NOT NULL,
-  Area varchar(300) NOT NULL,
-  Email varchar(300) NOT NULL,
-  Telefone varchar(200) NOT NULL,
-  PRIMARY KEY (Id)
-);
