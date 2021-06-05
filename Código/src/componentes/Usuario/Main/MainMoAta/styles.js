50914dbf51a2283acebdf24816fc5e7def86a03c
Pedro landim <69650012+Pedro-Landin@users.noreply.github.com>
2021-06-05 17:24:08 -0300

    Add files via upload

---
+++ Código/src/componentes/Usuario/Main/MainMoAta/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+   display: flex;
+  position:fixed;
+  
+`;
+
+
+export const Table = styled.div`
+  height: 27px;
+  width: 50rem;
+  left: 28rem;
+  top: 7rem;
+  background: #2f4050;
+  position: relative;
+`;
+
+export const ActionsAta= styled.div`
+   display: flex;
+ 
+
+  
+   >div{
+ position: relative;
+left: -21rem;
+top: 36rem;
+
+
+ >button{
+   background: #2f4050;
+  
+  color: aliceblue;
+  height: 25px;
+  border-radius: 4px;
+  width: 90px;
+  margin: 3px;
+
+  cursor: pointer;
+  
+  &:hover {
+   background: #6ADB9E;
+}
+   
+ }
+ }
+
+`;
+
+
+
+
+
