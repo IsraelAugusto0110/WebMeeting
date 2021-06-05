0447ab32631cd1c49b1e1ef3cf8dfc8b6383f55c
Pedro landim <69650012+Pedro-Landin@users.noreply.github.com>
2021-06-05 17:03:10 -0300

    Add files via upload

---
+++ Código/src/componentes/Login/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+//import Login from './index'
+//import Main from '../Gerente/Main/MainGer';
+//import Main from '../Usuario/Main/MainUsu';
+import Main from '../Administrador/Main/MainAdm';
+import GlobalStyles from '../styles/GlobalStyles';
+
+function App(){
+    return (
+      <>
+        <Main/>
+        <GlobalStyles />
+      </>
+    );
+}
+
+export default App;
---
+++ Código/src/componentes/Login/index.js
@@ -0,0 +1,119 @@
+import React, {useState}from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+   
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: '#6ADB9E',
+  },
+  form: {
+    width: '100%', 
+    marginTop: theme.spacing(2),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: '#2f4050',
+    
+  },
+}));
+
+
+
+
+export default function SignIn() {
+  const classes = useStyles();
+  
+ 
+  
+  
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5"></Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="nome"
+            label="Nome"
+            name="nome"
+            autoComplete="nome"
+            autoFocus
+          />
+
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+           
+          >
+            Entrar
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Esqueceu sua senha?
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}></Box>
+    </Container>
+  );
+}
