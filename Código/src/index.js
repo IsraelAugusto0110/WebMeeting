9fff7281e059f3ae17bb0d3d98e8b180ae66e367
Pedro landim <69650012+Pedro-Landin@users.noreply.github.com>
2021-06-05 16:29:49 -0300

    Add files via upload

---
+++ Código/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Pasta Login chama Adm ou Gerente, Usuario
+import App from './components/Login/App'
+
+
+
+ReactDOM.render(
+ 
+    <App />
+   
+  ,
+  document.getElementById('root')
+);
+
+
