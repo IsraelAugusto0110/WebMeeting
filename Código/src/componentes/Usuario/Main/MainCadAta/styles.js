41b4127585511ec8da9cf4b62eac6712307ee6d1
Pedro landim <69650012+Pedro-Landin@users.noreply.github.com>
2021-06-05 17:15:34 -0300

    Add files via upload

---
+++ Código/src/componentes/Usuario/Main/MainCadAta/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+   display: flex;
+  
+  
+`;
+
+
+export const Participantes = styled.div`
+ 
+
+ position: fixed; 
+ top: 2vw;
+ left: 75rem;
+
+ width: 11rem;
+
+`;
+
+
+
+
+
+
+
